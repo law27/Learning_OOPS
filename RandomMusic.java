ca146b0bab0a583e0d558c13c121fea4bc11086b
LawranceMichaelite <jeyalawrance@gmail.com>
2020-03-21 17:39:32 +0530

    Modified

--- RandomMusic.java
+++ RandomMusic.java
@@ -1,42 +1,42 @@
-package com.company;
+
 import javax.sound.midi.*;
-public class Main {
+
+public class RandomMusic {
 
     public static void main(String[] args) {
-        Main player = new Main();
-        int instrument = (int) (Math.random() * 128) ;
-        int note = (int) (Math.random() * 128) ;
-        player.play(instrument , note);
+        RandomMusic player = new RandomMusic();
+        int instrument = (int) (Math.random() * 128);
+        int note = (int) (Math.random() * 128);
+        player.play(instrument, note);
     }
 
-    public void play(int instrument, int note)
-    {
+    public void play(int instrument, int note) {
         try {
             Sequencer player = MidiSystem.getSequencer();
             player.open();
 
-            Sequence seq = new Sequence(Sequence.PPQ , 4);
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
 
-            Track track = seq.createTrack() ;
+            Track track = seq.createTrack();
 
-            ShortMessage newInstrument = new ShortMessage() ;
-            newInstrument.setMessage(192,1,instrument,0);
-            MidiEvent ins = new MidiEvent(newInstrument,1);
+            ShortMessage newInstrument = new ShortMessage();
+            newInstrument.setMessage(192, 1, instrument, 0);
+            MidiEvent ins = new MidiEvent(newInstrument, 1);
             track.add(ins);
 
-            ShortMessage a = new ShortMessage() ;
-            a.setMessage(144,1 , note , 100);
-            MidiEvent ins1 = new MidiEvent(a,1);
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, note, 100);
+            MidiEvent ins1 = new MidiEvent(a, 1);
             track.add(ins1);
 
-            ShortMessage b = new ShortMessage() ;
-            b.setMessage(198,1,note,100);
-            MidiEvent ins2 = new MidiEvent(b,16);
+            ShortMessage b = new ShortMessage();
+            b.setMessage(198, 1, note, 100);
+            MidiEvent ins2 = new MidiEvent(b, 16);
             track.add(ins2);
 
             player.setSequence(seq);
             player.start();
-        }catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
